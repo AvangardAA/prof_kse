1168387f2480896de83806e29c2f3e0294501f2a
Artem Nazarenko <artem.n@peanut.trade>
2024-06-16 20:39:57 +0300

    setup for task 2 tests

--- class/Class3.2/src/TaskTDD1.cpp
+++ class/Class3.2/src/TaskTDD1.cpp
@@ -2,6 +2,8 @@
 
 auto Triangle::get_area() -> double
 {
+    if (!(a + b > c && a + c > b && b + c > a)) {return -1;}
+
     double s = (a + b + c) / 2.0;
     return std::sqrt(s * (s - a) * (s - b) * (s - c));
 }
---
+++ class/Class3.2/src/TaskTDD2.cpp
@@ -0,0 +1,6 @@
+#include "TaskTDD2.hpp"
+
+auto RGB::get_magic_color() -> color
+{
+    return std::make_tuple(r,g,b);
+}
