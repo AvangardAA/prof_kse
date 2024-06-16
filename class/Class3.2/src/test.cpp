eb684db80040a5de9f8c3cebab4ae785e7eb1f75
Artem Nazarenko <artem.n@peanut.trade>
2024-06-16 20:39:57 +0300

    setup for task 2 tests

--- class/Class3.2/src/test.cpp
+++ class/Class3.2/src/test.cpp
@@ -1,12 +1,13 @@
 #include <print>
 #include <TaskTDD1.hpp>
+#include <TaskTDD2.hpp>
 #include <UnitTests.hpp>
 
 auto main() -> int 
 {
     UnitTests testSuite;
 
-    testSuite.addTest("TDD",
+    testSuite.addTest("Formula works",
     [](){
         // Build:
         Triangle triangle(3,4,5);
@@ -21,13 +22,39 @@ auto main() -> int
     testSuite.addTest("Formula ok, but triangle dont exist",
     [](){
         // Build:
-        Triangle triangle(3,4,5);
+        Triangle triangle(1,1,2);
 
         // Operate:
         auto result = triangle.get_area();
 
         // Check:
-        ASSERT_NEQ(result, 6)
+        ASSERT_NEQ(result, 0)
+    });
+
+    testSuite.addTest("Zero lengths",
+    [](){
+        // Build:
+        Triangle triangle(0,0,0);
+
+        // Operate:
+        auto result = triangle.get_area();
+
+        // Check:
+        ASSERT_NEQ(result, 0)
+    });
+
+    // Here we start second task tests
+
+    testSuite.addTest("Input incorrect",
+    [](){
+        // Build:
+        RGB rgb(0,0,0);
+
+        // Operate:
+        auto result = rgb.get_magic_color();
+
+        // Check:
+        ASSERT_NEQ(std::get<0>(result), 0)
     });
 
     testSuite.run();
