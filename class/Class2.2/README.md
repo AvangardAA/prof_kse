c85b826b467513e9a73e8c0b128731a5ebf727eb
Artem Nazarenko <artem.n@peanut.trade>
2024-07-04 16:33:44 +0300

    home 2 implementation
    - added cpp2 implementation of class task
    - added home task to cpp2 version (unfavorite color)
    - added autotests.sh script and files to test/verify both implementations

--- class/Class2.2/README.md
+++ class/Class2.2/README.md
@@ -1,8 +1,3 @@
-#### Please create 2 folders in this directory and put input file in ./local_files
+#### Please make compile.sh executable if its not on your machine
 
-```mkdir build```
-```mkdir local_files```
-
-#### Compile with
-
-```clang++ -Wall -Wextra -Wpedantic -Werror -std=c++23 main.cpp -o ./build/main```
+- ```chmod +x compile.sh```
---
+++ class/Class2.2/autotests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+file_path=(
+    "image1.txt"
+    "image2.txt"
+    "image3.txt"
+    "image4.txt"
+    "image5.txt"
+)
+
+if [ ! -f "./build/main" ]; then
+    echo "Compile first, then test."
+    exit 1
+fi
+
+run_test() {
+    local input_file="$1"
+    local next_str="$2"
+    local output_file="$3"
+    local expected_output_file="$4"
+    local test_name="$5"
+
+    ./build/main "$input_file" "$next_str" "$output_file"
+
+    if cmp -s "local_files/$output_file" "$expected_output_file"; then
+        echo -e "Test '${test_name}' \033[92mPASSED ✓\033[0m"
+    else
+        echo -e "Test '${test_name}' \033[91mFAILED ✗\033[0m"
+        echo "Expected output file: ${expected_output_file}"
+        echo "Actual output file:"
+        cat "local_files/$output_file"
+    fi
+}
+
+run_test "${file_path[0]}" "red" "out1.txt" "local_files/tests/out1_verify.txt" "1 Left column and third red"
+run_test "${file_path[1]}" "red" "out2.txt" "local_files/tests/out2_verify.txt" "2 Red hammer inside test"
+run_test "${file_path[2]}" "blue" "out3.txt" "local_files/tests/out3_verify.txt" "3 Blue left corner test"
+run_test "${file_path[3]}" "green" "out4.txt" "local_files/tests/out4_verify.txt" "4 Green wall inside test"
+run_test "${file_path[4]}" "green" "out5.txt" "local_files/tests/out5_verify.txt" "5 Green inside image"
