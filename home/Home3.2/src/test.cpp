e74a265accdd69795f595038f667c846b544e697
Artem Nazarenko <artem.n@peanut.trade>
2024-07-05 20:19:03 +0300

    ported class to home cpp2
    - where possible used cpp2 syntax
    - syntax of test.cpp and UnitTests.hpp is cpp1

---
+++ home/Home3.2/src/test.cpp
@@ -0,0 +1,100 @@
+#include <UnitTests.hpp>
+#include "TaskTDD1.h"
+#include "TaskTDD2.h"
+
+auto main() -> int 
+{
+    UnitTests testSuite;
+
+    testSuite.addTest("Formula works",
+    [](){
+        // Build:
+        Triangle triangle(3,4,5);
+
+        // Operate:
+        auto result = triangle.get_area();
+
+        // Check:
+        ASSERT_EQ(result, 6)
+    });
+
+    testSuite.addTest("Formula ok, but triangle dont exist",
+    [](){
+        // Build:
+        Triangle triangle(1,1,2);
+
+        // Operate:
+        auto result = triangle.get_area();
+
+        // Check:
+        ASSERT_NEQ(result, 0)
+    });
+
+    testSuite.addTest("Zero lengths",
+    [](){
+        // Build:
+        Triangle triangle(0,0,0);
+
+        // Operate:
+        auto result = triangle.get_area();
+
+        // Check:
+        ASSERT_NEQ(result, 0)
+    });
+
+    // Here we start second task tests
+
+    testSuite.addTest("Input incorrect",
+    [](){
+        // Build:
+        RGB rgb(0,0,0);
+
+        // Operate:
+        auto result = rgb.get_magic_color();
+
+        // Check:
+        ASSERT_EQ(std::get<0>(result), 0)
+        ASSERT_EQ(std::get<1>(result), 0)
+        ASSERT_EQ(std::get<2>(result), 0)
+    });
+
+    testSuite.addTest("R component incorrect",
+    [](){
+        // Build:
+        RGB rgb(0,100,100);
+
+        // Operate:
+        auto result = rgb.get_magic_color();
+
+        // Check:
+        ASSERT_NEQ(result, std::make_tuple(255,198,100))
+    });
+
+    testSuite.addTest("G component incorrect",
+    [](){
+        // Build:
+        RGB rgb(100,200,100);
+
+        // Operate:
+        auto result = rgb.get_magic_color();
+
+        // Check:
+        ASSERT_NEQ(result, std::make_tuple(49,142,100))
+    });
+
+    testSuite.addTest("B component incorrect",
+    [](){
+        // Build:
+        RGB rgb(100,100,100);
+
+        // Operate:
+        auto result = rgb.get_magic_color();
+
+        // Check:
+        ASSERT_EQ(std::get<0>(result), 49)
+        ASSERT_EQ(std::get<1>(result), 198)
+        ASSERT_EQ(std::get<2>(result), 100)
+    });
+
+    testSuite.run();
+}
